38b13d2f2cb59aadfadef46f681c142b5367935f
YaniAgoune <6256085@bdeb.qc.ca>
2024-11-14 10:10:26 -0500

    Added ground drag and speed limit to player movement

--- mmya-escapegame/Assets/Scenes/Scripts/PlayerMovement.cs
+++ mmya-escapegame/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
   [Header("Movement")]
 
   public float moveSpeed;
+    [Header("Ground Check")]
+    public float groundDrag;
+  public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+    
   public Transform orientation;
   float horizontalInput;
   float verticalInput;
@@ -22,6 +28,17 @@ public class PlayerMovement : MonoBehaviour
  void Update()
     {
         MyInput();
+        SpeedControl();
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight*0.5f +0.2f, whatIsGround);
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }else
+        {
+            rb.drag = 0;
+        }
     }
     private void FixedUpdate(){
         MovePlayer();
@@ -39,5 +56,13 @@ public class PlayerMovement : MonoBehaviour
 
         rb.AddForce(moveDirection.normalized * moveSpeed *10f, ForceMode.Force);
     }
-   
+   private void SpeedControl(){
+    Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+    if(flatVel.magnitude > moveSpeed)
+    {
+        Vector3 limitedVel = flatVel.normalized *moveSpeed;
+        rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+    }
+   }
 }
