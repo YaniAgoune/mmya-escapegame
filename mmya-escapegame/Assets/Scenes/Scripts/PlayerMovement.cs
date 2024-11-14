a7dca4d957cc7943b8342da973c923b9f4f0b67b
YaniAgoune <6256085@bdeb.qc.ca>
2024-11-14 10:52:11 -0500

    added jumping

--- mmya-escapegame/Assets/Scenes/Scripts/PlayerMovement.cs
+++ mmya-escapegame/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -7,8 +7,17 @@ public class PlayerMovement : MonoBehaviour
   [Header("Movement")]
 
   public float moveSpeed;
-    [Header("Ground Check")]
-    public float groundDrag;
+public float groundDrag;
+  public float jumpForce;
+  public float jumpCooldown;
+
+  public float airMultiplier; 
+  bool readyToJump;
+  [Header("Keybinds")]
+  public KeyCode jumpKey = KeyCode.Space;
+  
+  [Header("Ground Check")]
+  
   public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
@@ -24,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        readyToJump = true;
     }
  void Update()
     {
@@ -48,14 +58,27 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        if(Input.GetKey(jumpKey) && readyToJump && grounded){
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer(){
         // calculate movement direction, ensure moving to the direction player is looking
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
+        
+        
+        if (grounded){
         rb.AddForce(moveDirection.normalized * moveSpeed *10f, ForceMode.Force);
+        }
+        else if(!grounded){
+             rb.AddForce(moveDirection.normalized * moveSpeed *10f * airMultiplier, ForceMode.Force);
+        }
     }
+    
    private void SpeedControl(){
     Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
@@ -64,5 +87,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 limitedVel = flatVel.normalized *moveSpeed;
         rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
     }
+   }
+
+   private void Jump(){
+    //reset y velocity to 0
+    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+    //calls AddForce from rigid body, force is added upwards, uses impulse and not force since it's only once
+    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+   }
+
+   private void ResetJump(){
+    readyToJump = true;
    }
 }
